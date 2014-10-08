a500027dd9c6a3af9d6ea11e18324231ca6a320c
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-10-07 21:24:38 -0700

    refactored int56.c

--- src/fread_uint56_seq.c
+++ src/fread_uint56_seq.c
@@ -21,35 +21,8 @@ freely, subject to the following restrictions:
 #include <stdlib.h>
 #include <stdio.h>
 #include <libcortex.h>
-#define SetField(x,n) ((((uint56)a[x]) << n))
 
-void decompose_uint56_le(uint56 a, byte56_ptr b) {
-	b[0] = (byte)a;
-	b[1] = (byte)(a >> 8);
-	b[2] = (byte)(a >> 16);
-	b[3] = (byte)(a >> 24);
-	b[4] = (byte)(a >> 32);
-	b[5] = (byte)(a >> 40);
-	b[6] = (byte)(a >> 48);
-}
-uint56 compose_uint56_le(byte56_ptr a) {
-	return (SetField(6, 48) | SetField(5, 40) | SetField(4, 32) | SetField(3, 24) | SetField(2, 16) | SetField(1, 8) | SetField(0, 0)) & UINT56_MAX;
-}
-
-void decompose_uint56_be(uint56 a, byte56_ptr b) {
-	b[0] = (byte)(a >> 48);
-	b[1] = (byte)(a >> 40);
-	b[2] = (byte)(a >> 32);
-	b[3] = (byte)(a >> 24);
-	b[4] = (byte)(a >> 16);
-	b[5] = (byte)(a >> 8);
-	b[6] = (byte)a;
-}
-uint56 compose_uint56_be(byte56_ptr a) {
-	return (SetField(0, 48) | SetField(1, 40) | SetField(2, 32) | SetField(3, 24) | SetField(4, 16) | SetField(5, 8) | SetField(6, 0))  & UINT56_MAX;
-}
-static int fread_uint56_base(FILE*, byte56_ptr); 
-int fread_uint56_base(FILE* a, byte56_ptr b) {
+int fread_uint56_seq(FILE* a, byte56_ptr b) {
 	int r0, r1, r2, r3, r4, r5, r6;
 	// endianness is not important at this point as we are just reading bytes
 	r0 = fgetc(a);
@@ -72,23 +45,3 @@ int fread_uint56_base(FILE* a, byte56_ptr b) {
 		return 1;
 	}
 }
-int fread_uint56_le(FILE* a, uint56_ptr b) {
-	int result;
-	byte56 container;
-	result = fread_uint56_base(a, container);
-	if(result) {
-		*b = compose_uint56_le(container);
-	}
-	return result;
-}
-
-int fread_uint56_be(FILE* a, uint56_ptr b) {
-	int result;
-	byte56 container;
-	result = fread_uint56_base(a, container);
-	if(result) {
-		*b = compose_uint56_be(container);
-	}
-	return result;
-}
-#undef SetField
