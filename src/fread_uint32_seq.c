e00a1c0bae7eb116bdff96f07f05e75cb5e3103a
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-10-07 21:23:45 -0700

    refactored int32

--- src/fread_uint32_seq.c
+++ src/fread_uint32_seq.c
@@ -21,28 +21,8 @@ freely, subject to the following restrictions:
 #include <stdlib.h>
 #include <stdio.h>
 #include <libcortex.h>
-#define SetField(x,n) ((((uint32)a[x]) << n))
-void decompose_uint32_le(uint32 a, byte32_ptr b) {
-	b[0] = (byte)a;
-	b[1] = (byte)(a >> 8);
-	b[2] = (byte)(a >> 16);
-	b[3] = (byte)(a >> 24);
-}
-uint32 compose_uint32_le(byte32_ptr a) {
-	return	SetField(3, 24) | SetField(2, 16) | SetField(1, 8) | SetField(0, 0);
-}
 
-void decompose_uint32_be(uint32 a, byte32_ptr b) {
-	b[3] = (byte)a;
-	b[2] = (byte)(a >> 8);
-	b[1] = (byte)(a >> 16);
-	b[0] = (byte)(a >> 24);
-}
-uint32 compose_uint32_be(byte32_ptr a) {
-	return	SetField(0, 24) | SetField(1, 16) | SetField(2, 8) | SetField(3, 0);
-}
-static int fread_uint32_base(FILE*, byte32_ptr); 
-int fread_uint32_base(FILE* a, byte32_ptr b) {
+int fread_uint32_seq(FILE* a, byte32_ptr b) {
 	int r0, r1, r2, r3;
 	// endianness is not important at this point as we are just reading bytes
 	r0 = fgetc(a);
@@ -59,24 +39,3 @@ int fread_uint32_base(FILE* a, byte32_ptr b) {
 		return 1;
 	}
 }
-int fread_uint32_le(FILE* a, uint32_ptr b) {
-	int result;
-	byte32 container;
-	result = fread_uint32_base(a, container);
-	if(result) {
-		*b = compose_uint32_le(container);
-	}
-	return result;
-}
-
-int fread_uint32_be(FILE* a, uint32_ptr b) {
-	int result;
-	byte32 container;
-	result = fread_uint32_base(a, container);
-	if(result) {
-		*b = compose_uint32_be(container);
-	}
-	return result;
-}
-#undef SetField
-
