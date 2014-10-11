ffda4cb8f296cef8a2c94de120cb14e602c6b3c3
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-10-11 16:46:02 -0700

    Updated libcortex.h to use byte* instead of separate type defs for many functions

--- include/libcortex.h
+++ include/libcortex.h
@@ -55,14 +55,6 @@ typedef byte byte48[6];
 typedef byte byte56[7];
 typedef byte byte64[8];
 
-typedef byte* byte16_ptr;
-typedef byte* byte24_ptr;
-typedef byte* byte32_ptr;
-typedef byte* byte40_ptr;
-typedef byte* byte48_ptr;
-typedef byte* byte56_ptr;
-typedef byte* byte64_ptr;
-
 /* newtypesizes typesize limits */
 enum {
 	UINT24_MAX = 0x00FFFFFF,
@@ -86,82 +78,82 @@ enum {
 //TODO: add functions for manipulating off boundary values
 
 
-void decompose_uint16_le(uint16, byte16_ptr);
-uint16 compose_uint16_le(byte16_ptr);
+void decompose_uint16_le(uint16, byte*);
+uint16 compose_uint16_le(byte*);
 
-void decompose_uint16_be(uint16, byte16_ptr);
-uint16 compose_uint16_be(byte16_ptr);
+void decompose_uint16_be(uint16, byte*);
+uint16 compose_uint16_be(byte*);
 
 int fread_uint16_le(FILE*, uint16_ptr);
 int fread_uint16_be(FILE*, uint16_ptr);
 
 /* 24-bit operations */
 
-void decompose_uint24_le(uint24, byte24_ptr);
-uint24 compose_uint24_le(byte24_ptr);
+void decompose_uint24_le(uint24, byte*);
+uint24 compose_uint24_le(byte*);
 
-void decompose_uint24_be(uint24, byte24_ptr);
-uint24 compose_uint24_be(byte24_ptr);
+void decompose_uint24_be(uint24, byte*);
+uint24 compose_uint24_be(byte*);
 
 int fread_uint24_le(FILE*, uint24_ptr);
 int fread_uint24_be(FILE*, uint24_ptr);
 
 /* 32-bit operations */
-uint32 compose_uint32_le(byte32_ptr);
-void decompose_uint32_le(uint32, byte32_ptr);
+uint32 compose_uint32_le(byte*);
+void decompose_uint32_le(uint32, byte*);
 
-uint32 compose_uint32_be(byte32_ptr);
-void decompose_uint32_be(uint32, byte32_ptr);
+uint32 compose_uint32_be(byte*);
+void decompose_uint32_be(uint32, byte*);
 
 int fread_uint32_le(FILE*, uint32_ptr);
 int fread_uint32_be(FILE*, uint32_ptr);
 /* 40-bit operations */
-uint40 compose_uint40_le(byte40_ptr);
-void decompose_uint40_le(uint40, byte40_ptr);
+uint40 compose_uint40_le(byte*);
+void decompose_uint40_le(uint40, byte*);
 
-uint40 compose_uint40_be(byte40_ptr);
-void decompose_uint40_be(uint40, byte40_ptr);
+uint40 compose_uint40_be(byte*);
+void decompose_uint40_be(uint40, byte*);
 
 int fread_uint40_le(FILE*, uint40_ptr);
 int fread_uint40_be(FILE*, uint40_ptr);
 
 /* 48-bit operations */
-uint48 compose_uint48_le(byte48_ptr);
-void decompose_uint48_le(uint48, byte48_ptr);
+uint48 compose_uint48_le(byte*);
+void decompose_uint48_le(uint48, byte*);
 
-uint48 compose_uint48_be(byte48_ptr);
-void decompose_uint48_be(uint48, byte48_ptr);
+uint48 compose_uint48_be(byte*);
+void decompose_uint48_be(uint48, byte*);
 
 int fread_uint48_le(FILE*, uint48_ptr);
 int fread_uint48_be(FILE*, uint48_ptr);
 
 /* 56-bit operations */
-uint56 compose_uint56_le(byte56_ptr);
-void decompose_uint56_le(uint56, byte56_ptr);
+uint56 compose_uint56_le(byte*);
+void decompose_uint56_le(uint56, byte*);
 
-uint56 compose_uint56_be(byte56_ptr);
-void decompose_uint56_be(uint56, byte56_ptr);
+uint56 compose_uint56_be(byte*);
+void decompose_uint56_be(uint56, byte*);
 
 int fread_uint56_le(FILE*, uint56_ptr);
 int fread_uint56_be(FILE*, uint56_ptr);
 /* 64-bit operations */
-uint64 compose_uint64_le(byte64_ptr);
-void decompose_uint64_le(uint64, byte64_ptr);
+uint64 compose_uint64_le(byte*);
+void decompose_uint64_le(uint64, byte*);
 
-uint64 compose_uint64_be(byte64_ptr);
-void decompose_uint64_be(uint64, byte64_ptr);
+uint64 compose_uint64_be(byte*);
+void decompose_uint64_be(uint64, byte*);
 
 int fread_uint64_le(FILE*, uint64_ptr);
 int fread_uint64_be(FILE*, uint64_ptr);
 
 /* endian ignorant functions - read in sequentially */
-int fread_uint16_seq(FILE*, byte16_ptr);
-int fread_uint24_seq(FILE*, byte24_ptr);
-int fread_uint32_seq(FILE*, byte32_ptr);
-int fread_uint40_seq(FILE*, byte40_ptr);
-int fread_uint48_seq(FILE*, byte48_ptr);
-int fread_uint56_seq(FILE*, byte56_ptr);
-int fread_uint64_seq(FILE*, byte64_ptr);
+int fread_uint16_seq(FILE*, byte*);
+int fread_uint24_seq(FILE*, byte*);
+int fread_uint32_seq(FILE*, byte*);
+int fread_uint40_seq(FILE*, byte*);
+int fread_uint48_seq(FILE*, byte*);
+int fread_uint56_seq(FILE*, byte*);
+int fread_uint64_seq(FILE*, byte*);
 
 
 /* File operations */
